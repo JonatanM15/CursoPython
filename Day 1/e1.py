a248643df7d8b6b7761bd50ee26fdce9b4470fb3
jonatan090401@gmail.com <JonatanM15>
2021-11-15 09:22:28 -0600

    What i made in day 1

---
+++ Day 1/e1.py
@@ -0,0 +1,27 @@
+#int-entera
+a = 2
+#float- flotante 
+b = 2.3 
+#string- cadena de texto 
+c = "Hello world"
+#boolean
+d = True
+e = False 
+
+#imprimir variable 
+print(c)
+
+x = 1
+x = x + 1
+print(x)
+
+#Type data 
+city = input("type the city wheren you grew up:   ")
+#print (type(city))
+
+num1 = int(input("type your firs number:  "))
+#num1 = int(input)
+
+num2 = int(input("Type your second number "))
+sum = num1 + num2
+print(f"sum = {sum}")
---
+++ Day 1/e2.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("what is the name of the city where you grew up in? ")
+pet = input("what is your pet is name ")
+print(f"your band name could be {city} {pet}")
